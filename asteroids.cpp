298bf77962cfb5c4a7c86b2592a61c7ced9c2bef
Isaac <abhisaac@gmail.com>
2023-08-07 09:18:31 +0530

    rotating textures

--- asteroids.cpp
+++ asteroids.cpp
@@ -4,7 +4,7 @@
 /*
 TODOs
 1. 
-2. texture
+2. sp tinting instead of loading multiple enemies
 3. 
 */
 
@@ -12,23 +12,25 @@ TODOs
 #include <iostream>
 #include <vector>
 struct GameConstants {
-  inline static const float SIZEX = 800.f;
-  inline static const float SIZEY = 450.f;
-  inline static const float DELTA = 1.4f;
+  inline static const float SIZEX = 600.f;
+  inline static const float SIZEY = 750.f;
+  inline static const float DELTA = 0.4f;
+  inline static const float DELTA1 = 3.4f;
 };
 
 Vector2 randDelta() {
   return { rand()%2 ? -GameConstants::DELTA : GameConstants::DELTA,
-           GameConstants::DELTA};
+           GameConstants::DELTA1};
   // return {(float)rand()*GameConstants::DELTA/ (float)(RAND_MAX) - GameConstants::DELTA, 
   //         (float)rand()*GameConstants::DELTA/ (float)(RAND_MAX) - GameConstants::DELTA};
 }
 Vector2 randPosition() {
   // respawn at the top 1/8th of the window
   return {(float)rand()*GameConstants::SIZEX/ (float)(RAND_MAX),  
-          (float)rand()*GameConstants::SIZEY/ (float)(8.0f * RAND_MAX)};
+          (float)rand()*GameConstants::SIZEY/ (float)(16.0f * RAND_MAX)};
 }
 
+// SHIP
 struct Sprite
 {
   Sprite(char *name)
@@ -49,8 +51,8 @@ struct Sprite
     
     if (pos.x > GameConstants::SIZEX) pos.x = 0.f;
     if (pos.x < 0.f) pos.x = GameConstants::SIZEX;
-    if (pos.y > GameConstants::SIZEY) pos.y = 0.f;
-    if (pos.y < 0.f) pos.y = GameConstants::SIZEY;
+    if (pos.y > GameConstants::SIZEY-70.f) pos.y = GameConstants::SIZEY-70.f;
+    if (pos.y < 0.f) pos.y = 0.f;
   }
   ~Sprite()
   {
@@ -59,40 +61,54 @@ struct Sprite
   Texture2D sp;
   Vector2 pos;
 };
+
+
 long long cnt = 0L;
+Texture2D enemyTex[5];
+
+// ENEMY
 struct Enemy {
   Enemy(Texture2D& sp, Vector2 pos) : 
-        sp(sp), pos(pos), delta(randDelta()), destroyed(false), offset(0.f),
-        size(1.f)
+        sp(sp)
   {
     // puts("EE");
+    reset();
   }
   void draw()
   {
+    rot += 0.5f;
     if (destroyed) {
-      offset += 1.4f;
+      offset += 1.4f;  
       size -= 0.02f;
       // DrawCircle(pos.x - offset*2.f, pos.y - offset, size, GREEN);
       // DrawCircl e(pos.x - offset, pos.y + offset, size, GREEN);
       // DrawCircle(pos.x + offset, pos.y, size, GREEN);
       if (size > 0.f) {
         DrawTextureEx(sp, {pos.x - offset*2.f, pos.y- offset}, -offset, size, WHITE);
-      DrawTextureEx(sp, {pos.x - offset, pos.y}, offset, size, WHITE);
-      DrawTextureEx(sp, {pos.x + offset, pos.y}, -offset, size, WHITE);  
+        DrawTextureEx(sp, {pos.x - offset, pos.y}, offset, size, WHITE);
+        DrawTextureEx(sp, {pos.x + offset, pos.y}, -offset, size, WHITE);  
       } else {
         reset();
       }
       
     } else {
-      DrawTexture(sp, pos.x, pos.y, WHITE);
+      // DrawTextureEx(sp, {pos.x, pos.y}, rot, 1.0f, WHITE);
+      DrawTexturePro(sp, { 0.0f, 0.0f, (float)sp.width, (float)sp.height }, 
+        { pos.x, pos.y, (float)sp.width, (float)sp.height }, 
+        {sp.width / 2.f, sp.height / 2.f}, dir ? -rot : rot, WHITE);
+//      
+      // DrawRectangle(pos.x, pos.y, 64, 64, ORANGE); // Test Bounding box of Texture.
     }
   }
   void reset() {
+    sp = enemyTex[rand()%5];  
     pos = randPosition();
           delta = randDelta(); 
     offset = 0.f;
     destroyed = false;
     size = 1.f;
+    rot = 0.0f;
+    dir = rand()  %2 ;
   }
   void update() {
     pos.x += delta.x;
@@ -104,12 +120,14 @@ struct Enemy {
           // ++cnt;
         } 
   }
+  bool dir;
   Texture2D sp;
   Vector2 pos;
   Vector2 delta;
   bool destroyed;
   float offset;
   float size;
+  float rot;
 };
 
  int b_idx = 0;
@@ -136,14 +154,19 @@ int main()
   long long int elapsed = 0;
   
   InitWindow(GameConstants::SIZEX, GameConstants::SIZEY, "!! Asteroids");
-  Texture2D enemyTex = LoadTexture("e.png");
+  
+  enemyTex[0] = LoadTexture("e1.png");
+  enemyTex[1] = LoadTexture("e2.png");
+  enemyTex[2] = LoadTexture("e3.png");
+  enemyTex[3] = LoadTexture("e4.png");
+  enemyTex[4] = LoadTexture("e5.png");
   SetTargetFPS(60);
   
   // Enemies
   int N = 4;
   std::vector<Enemy> badguys;
   for (int i = 0; i< N; ++i) 
-    badguys.emplace_back(enemyTex, randPosition());
+    badguys.emplace_back(enemyTex[rand()%5], randPosition());
     
 
   // Bullets
@@ -152,7 +175,7 @@ int main()
  
   
   {
-    Sprite ship("x.png");
+    Sprite ship("fighter.png");
     bool gameover = false;
     while (!WindowShouldClose())
     {
@@ -160,8 +183,8 @@ int main()
       BeginDrawing();
         ClearBackground(BLACK);
         if (gameover) {
-          DrawText("GAME OVER", GameConstants::SIZEX/2 - 20, GameConstants::SIZEY/2, 20, WHITE);
-          DrawText("Press S to start", GameConstants::SIZEX/2 - 20, GameConstants::SIZEX/2 + 10, 10, WHITE);
+          DrawText("GAME OVER", GameConstants::SIZEX/3 - 20, GameConstants::SIZEY/2, 40, WHITE);
+          DrawText("Press S to start", GameConstants::SIZEX/3 - 20, GameConstants::SIZEY/2 + 50, 20, WHITE);
           if (IsKeyDown(KEY_S)) {
               gameover = false;
               cnt = 0;
@@ -181,8 +204,15 @@ int main()
         for (auto& e : badguys) {
           e.draw();
           e.update();
-          if (!e.destroyed && CheckCollisionRecs({e.pos.x, e.pos.y, 24.f, 24.f},
-                                 {ship.pos.x, ship.pos.y, 70.f, 70.f})) {
+          
+
+
+          if (!e.destroyed && 
+          CheckCollisionCircleRec({e.pos.x, e.pos.y}, e.sp.width/2, 
+                {ship.pos.x, ship.pos.y, (float)ship.sp.width, (float)ship.sp.height}))
+          // CheckCollisionRecs({e.pos.x, e.pos.y, 50.f, 50.f},
+          //                        {ship.pos.x, ship.pos.y, 70.f, 70.f})) 
+          {
             gameover = true;
           }
         }
@@ -201,15 +231,16 @@ int main()
             bullets[i].valid = true;
           }
         }
-        // TODO: disable bullet after collision
+        
           for(int i =0 ;i<500;++i) {
             bullets[i].draw();
             bullets[i].update();
             if (!bullets[i].valid) continue;
             for (auto& e : badguys) {
-              if (CheckCollisionPointRec(bullets[i].pos, 
-                {e.pos.x, e.pos.y, 80.f, 80.f})) {
-                  ++cnt;
+              if (CheckCollisionPointCircle(bullets[i].pos, {e.pos.x, e.pos.y}, e.sp.width/2))
+              // if (CheckCollisionPointRec(bullets[i].pos, 
+              //   {e.pos.x, e.pos.y, 80.f, 80.f})) {
+              {    ++cnt;
                   // e.reset();
                   e.destroyed = true;
                   bullets[i].valid = false;
@@ -224,7 +255,11 @@ int main()
       EndDrawing();
     }
   }
-  UnloadTexture(enemyTex);
+  UnloadTexture(enemyTex[0]);
+  UnloadTexture(enemyTex[1]);
+  UnloadTexture(enemyTex[2]);
+  UnloadTexture(enemyTex[3]);
+  UnloadTexture(enemyTex[4]);
   CloseWindow();
   return 0;
 }
