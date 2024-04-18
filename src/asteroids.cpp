2fe61a247b2bc28fa8fe6ac19c86b251305f2e2b
Isaac <abhisaac@gmail.com>
2024-04-19 01:05:30 +0530

    better explosions! boom

--- src/asteroids.cpp
+++ src/asteroids.cpp
@@ -4,7 +4,9 @@
 /*
 TODOs
 1. game mechanics
-2. explosion animation can be better
+2. clean up
+3. refactor explosion into a common header
+4. 
 */
 
 #include "raylib.h"
@@ -40,10 +42,10 @@ struct Sprite
   }
   void update() {
     const float updateBy = 5.0f;
-    if (IsKeyDown(KEY_RIGHT)) pos.x += updateBy;
-    if (IsKeyDown(KEY_LEFT)) pos.x -= updateBy;
-    if (IsKeyDown(KEY_UP)) pos.y -= updateBy;
-    if (IsKeyDown(KEY_DOWN)) pos.y += updateBy;
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) pos.x += updateBy;
+    if (IsKeyDown(KEY_LEFT)|| IsKeyDown(KEY_A)) pos.x -= updateBy;
+    if (IsKeyDown(KEY_UP)|| IsKeyDown(KEY_W)) pos.y -= updateBy;
+    if (IsKeyDown(KEY_DOWN)|| IsKeyDown(KEY_S)) pos.y += updateBy;
     
     if (pos.x > GameConstants::SIZEX) pos.x = 0.f;
     if (pos.x < 0.f) pos.x = GameConstants::SIZEX;
@@ -62,6 +64,60 @@ struct Sprite
 long long cnt = 0L;
 Texture2D enemyTex;
 
+struct ExplosionParticle
+{
+    Vector2 position;
+    Vector2 velocity;
+    float size;
+};
+bool showExplosion = true;
+struct Explosion {
+    std::vector<ExplosionParticle> particles;
+    bool valid = false;
+    const int numParticles = 60;
+    const float explosionSpeed = .3f; // Adjust this value for slower/faster explosion
+    Color tint;
+    
+    void setup(float x, float y, Color c) {
+        tint = c;
+        particles.clear();
+        valid = true;
+        for (int i = 0; i < numParticles; ++i) {
+            ExplosionParticle particle;
+            particle.position = {x, y}; //{GetMouseX(), GetMouseY()};
+            particle.velocity = {(float)GetRandomValue(-10, 10), (float)GetRandomValue(-10, 10)};
+            particle.size = 9.f;
+            particles.push_back(particle);
+        }
+    }
+
+    void update() {
+        if (showExplosion && valid) {
+            for (auto &particle : particles)
+            {
+                particle.position.x += particle.velocity.x * explosionSpeed;
+                particle.position.y += particle.velocity.y * explosionSpeed;
+                particle.size -= (float)GetRandomValue(30, 70)/100;
+            }
+        }
+    }
+
+    bool draw() {
+        if (showExplosion && valid) {
+            int cnt = 0;
+            for (auto &particle : particles)
+            {
+                if (particle.size < 0.01f) {
+                    ++cnt;
+                }
+                DrawCircle(particle.position.x, particle.position.y, particle.size, tint);
+            }
+            if (cnt == particles.size()) {valid = false; particles.clear(); return false;}
+        }
+        return true;
+    }
+};
+
 // ENEMY
 struct Enemy {
   Enemy(Texture2D& sp, Vector2 pos, Color tint) : 
@@ -74,19 +130,21 @@ struct Enemy {
   {
     rot += 0.5f;
     if (destroyed) {
-      offset += 0.4f;  
-      size -= 0.02f;
-      if (size > 0.f) {
-        DrawTextureEx(sp, {pos.x - offset*2.f, pos.y- offset}, -offset, size, tint);
-        DrawTextureEx(sp, {pos.x - offset, pos.y}, offset, size, tint);
-        DrawTextureEx(sp, {pos.x + offset, pos.y}, -offset, size, tint);  
-      } else {
+      // offset += 0.4f;  
+      // size -= 0.02f;
+      // if (size > 0.f) {
+      //   DrawTextureEx(sp, {pos.x - offset*2.f, pos.y- offset}, -offset, size, tint);
+      //   DrawTextureEx(sp, {pos.x - offset, pos.y}, offset, size, tint);
+      //   DrawTextureEx(sp, {pos.x + offset, pos.y}, -offset, size, tint);  
+      // } else {
+      //   reset();
+      // }
+      if (!explosion.draw()) {
         reset();
       }
-      
     } else {
       DrawTexturePro(sp, { 0.0f, 0.0f, (float)sp.width, (float)sp.height }, 
-        { pos.x, pos.y, (float)sp.width, (float)sp.height }, 
+        { pos.x, pos.y, (float)sp.width*fullsize, (float)sp.height*fullsize }, 
         {sp.width / 2.f, sp.height / 2.f}, dir ? -rot : rot, tint);
     }
   }
@@ -98,12 +156,19 @@ struct Enemy {
     delta = randDelta(); 
     offset = 0.f;
     destroyed = false;
-    size = 1.f;
+    fullsize = (rand()%5)*.2f + .6f;
+    // std::cout << fullsize << std::endl;
+    size = fullsize;
+    
     rot = 0.0f;
     dir = rand()  %2 ;
   }
   
   void update() {
+    if (destroyed) {
+      explosion.update();
+      return;
+    }
     pos.x += delta.x;
     pos.y += delta.y;
 
@@ -112,8 +177,9 @@ struct Enemy {
     {
       reset();
     } 
-  }
 
+  }
+  Explosion explosion;
   bool dir;
   Texture2D sp;
   Vector2 pos;
@@ -121,6 +187,7 @@ struct Enemy {
   bool destroyed;
   float offset;
   float size;
+  float fullsize;
   float rot;
   Color tint;
 };
@@ -183,8 +250,8 @@ int main()
         ClearBackground(GRAY);
         if (gameover) {
           DrawText("GAME OVER", GameConstants::SIZEX/3 - 20, GameConstants::SIZEY/2, 40, WHITE);
-          DrawText("Press S to start", GameConstants::SIZEX/3 - 20, GameConstants::SIZEY/2 + 50, 20, WHITE);
-          if (IsKeyDown(KEY_S)) {
+          DrawText("Press R to Restart", GameConstants::SIZEX/3 - 20, GameConstants::SIZEY/2 + 50, 20, WHITE);
+          if (IsKeyDown(KEY_R)) {
               gameover = false;
               cnt = 0;
           }
@@ -232,9 +299,10 @@ int main()
             bullets[i].update();
             if (!bullets[i].valid) continue;
             for (auto& e : badguys) {
-              if (CheckCollisionPointCircle(bullets[i].pos, {e.pos.x, e.pos.y}, e.sp.width/2))
+              if (!e.destroyed && CheckCollisionPointCircle(bullets[i].pos, {e.pos.x, e.pos.y}, e.sp.width/2))
               {    ++cnt;
                   e.destroyed = true;
+                  e.explosion.setup(e.pos.x, e.pos.y, e.tint);
                   bullets[i].valid = false;
               }
             }
